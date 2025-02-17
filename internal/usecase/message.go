b0c9487adfe98305cf4cbfc656cf56ffd08eb7bd
kasderooi <kasderooij@gmail.com>
2025-02-17 20:31:28 +0100

    formatted the message better

--- internal/usecase/message.go
+++ internal/usecase/message.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	base = "%snHoi ik gebruik geen WhatsApp meer, je kan mij bereiken op Signal. https://signal.org/download/"
-	auto = "* Automated response *"
+	base = "%s/nHoi ik gebruik geen WhatsApp meer, je kan mij bereiken op Signal. \n https://signal.org/download/"
+	auto = "· _Automated response_ ·"
 )
 
 // HandleMessage processes incoming messages
